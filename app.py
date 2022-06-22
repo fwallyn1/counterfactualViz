b035834b73f90e5818e5c318b11d526703d20bee
Francois Wallyn <fwallyn@ad.inria.fr>
2022-06-22 17:15:51 +0200

    change dataset example

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import json
 app = Flask(__name__)
 
-DATASET_PATH = 'data_churn.json'
+DATASET_PATH = 'data_adult.json'
 @app.route('/')
 def index():
     return render_template('index.html')
