33e3f7925296d9ed53b0e3795dbdbf73918b7b7d
Francois Wallyn <fwallyn@ad.inria.fr>
2022-06-30 16:46:52 +0200

    start scatter plot

--- static/js/first_page/index_first_page.js
+++ static/js/first_page/index_first_page.js
@@ -6,7 +6,8 @@ function run(datasets) {
     var data_info = computeDataInfo(datasets[0]);
     var dataset = addChangesInfo(datasets[0]);
     draw_predict_class_circle(dataset["0.0"].y_x[0],"x",dataset[prob].y_true_x[0]);
+    draw_percent_bar(dataset["0.0"].proba_x[0])
     makeSelectFirstPage(dataset,data_info);
-
+    drawScatterPlot(dataset,data_info)
 }
 
